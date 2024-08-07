952afd3861c50d8a1f95893958aa122988d0161f
QuangAnh039 <quanganhn042@gmail.com>
2024-08-07 12:52:58 +0700

    complete

--- chat_application/src/peers.c
+++ chat_application/src/peers.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
 
 #define MAX_CONNECTIONS 10
 #define BUFFER_SIZE 1024
@@ -16,13 +18,18 @@ typedef struct
 
 connection_t connections[MAX_CONNECTIONS];
 int connection_count = 0;
+int listening_port;
 pthread_mutex_t lock;
+int yes = 1;
 
 void *handle_connection(void *ptr);
 void *input_handler(void *ptr);
 void list_connections();
 void terminate_connection(int id);
 void send_message(int id, const char *message);
+void show_myip();
+void show_myport();
+void connect_to_peers(const char *ip, int ports);
 
 int main(int argc, char *argv[]) 
 {
@@ -33,6 +40,7 @@ int main(int argc, char *argv[])
     }
 
     int port = atoi(argv[1]);
+    listening_port = port;
     int server_sock;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
@@ -58,6 +66,12 @@ int main(int argc, char *argv[])
     server_addr.sin_port = htons(port);
     memset(&(server_addr.sin_zero), '\0', 8);
 
+    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) 
+    {
+		perror("setsockopt");
+		exit(1);
+	}
+        
     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) 
     {
         perror("bind");
@@ -85,6 +99,9 @@ int main(int argc, char *argv[])
             perror("accept");
             continue;
         }
+
+        printf("Accepted a new connection from address %s, setup at port %d\n", inet_ntoa(client_addr.sin_addr),  ntohs(client_addr.sin_port));
+
         pthread_mutex_lock(&lock);
         if (connection_count < MAX_CONNECTIONS) 
         {
@@ -116,11 +133,13 @@ void *handle_connection(void *ptr)
     connection_t *conn = (connection_t *)ptr;
     char buffer[BUFFER_SIZE];
     int bytes_read;
+    struct sockaddr_in local_addr;
+    socklen_t addr_len = sizeof(local_addr);
 
     while ((bytes_read = recv(conn->sock, buffer, BUFFER_SIZE - 1, 0)) > 0) 
     {
         buffer[bytes_read] = '\0';
-        printf("Received from %s:%d - %s\n", inet_ntoa(conn->address.sin_addr), ntohs(conn->address.sin_port), buffer);
+        printf("Received from ip: %s port: %d - content: %s\n", inet_ntoa(conn->address.sin_addr), ntohs(conn->address.sin_port), buffer);
     }
 
     close(conn->sock);
@@ -136,7 +155,7 @@ void *input_handler(void *ptr)
     char command[BUFFER_SIZE];
     char message[BUFFER_SIZE];
     int id;
-
+    unsigned int ports;
     while(1)
     {
         printf("> ");
@@ -156,15 +175,15 @@ void *input_handler(void *ptr)
         }
         else if(strncmp(command, "myip", 4) == 0)
         {
-
+            show_myip();
         }
         else if(strncmp(command, "myport", 6) == 0)
         {
-
+            show_myport();
         }
-        else if(sscanf(command, "connect %s %d", message, &id) == 2)
+        else if(sscanf(command, "connect %s %d", message, &ports) == 2)
         {
-
+            connect_to_peers(message, ports);
         }
         else if(strncmp(command, "list", 4) == 0)
         {
@@ -187,7 +206,11 @@ void *input_handler(void *ptr)
             }
             exit(0);
         }
-        else 
+        else if(strncmp(command, "\n", 1) == 0)
+        {
+            //do nothing
+        }
+        else
         {
             printf("Unknown command. Type 'help' for list of commands.\n");
         }
@@ -196,13 +219,15 @@ void *input_handler(void *ptr)
 
 void list_connections()
 {
+    printf("\n*****************************************\n");
     pthread_mutex_lock(&lock);
-    printf("ID\tIP\tPort\n");
+    printf("ID\tIP\t\tPort\n");
     for(int i = 0; i < connection_count; i++)
     {
-        printf("%d\t%s\t%d\n", i, inet_ntoa(connections[i].address.sin_addr), ntohs(connections[i].address.sin_port));
+        printf("%d|\t%s\t|%d\n", i, inet_ntoa(connections[i].address.sin_addr), ntohs(connections[i].address.sin_port));
     }
-    pthread_muxtex_unlock(&lock);
+    pthread_mutex_unlock(&lock);
+    printf("*****************************************\n");
 }
 
 void terminate_connection(int id)
@@ -233,4 +258,97 @@ void send_message(int id, const char *message)
 
     send(connections[id].sock, message, strlen(message), 0);
     printf("Message sent to connection %d.\n", id);
+}
+
+void show_myip() 
+{
+    struct ifaddrs *ifaddr, *ifa;
+    char ip[INET_ADDRSTRLEN];
+
+    if (getifaddrs(&ifaddr) == -1) 
+    {
+        perror("getifaddrs");
+        return;
+    }
+
+    printf("IP addresses:\n");
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) 
+    {
+        if (ifa->ifa_addr == NULL)
+        {
+            continue;
+        }
+        if (ifa->ifa_addr->sa_family == AF_INET) 
+        {
+            if (inet_ntop(AF_INET, &((struct sockaddr_in *)ifa->ifa_addr)->sin_addr, ip, sizeof(ip)) != NULL) 
+            {
+                printf("%s: %s\n", ifa->ifa_name, ip);
+            } 
+            else 
+            {
+                perror("inet_ntop");
+            }
+        }
+    }
+    freeifaddrs(ifaddr);
+}
+
+void show_myport() 
+{
+    printf("Listening on port: %d\n", listening_port);
+}
+
+void connect_to_peers(const char *ip, int ports)
+{
+    int sock;
+    struct sockaddr_in server_addr;
+
+    // create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    {
+        perror("socket");
+        return;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(ports);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
+
+    // connect to peer
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) 
+    {
+        perror("connect");
+        close(sock);
+        return;
+    }
+
+    // Add peers into list
+    pthread_mutex_lock(&lock);
+
+    if (connection_count < MAX_CONNECTIONS) 
+    {
+        connections[connection_count].sock = sock;
+        connections[connection_count].address.sin_family = AF_INET;
+        connections[connection_count].address.sin_port = htons(ports);
+        inet_pton(AF_INET, ip, &connections[connection_count].address.sin_addr);
+
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, handle_connection, &connections[connection_count]) != 0) 
+        {
+            perror("pthread_create");
+        } 
+        else 
+        {
+            connection_count++;
+            printf("Connected to %s:%d\n", ip, ports);
+        }
+    } 
+    else 
+    {
+        close(sock);
+        printf("Max connections reached. Connection to %s:%d rejected.\n", ip, ports);
+    }
+
+    pthread_mutex_unlock(&lock);
 }
